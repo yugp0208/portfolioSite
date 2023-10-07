4450b11de26b3cd30e6c3b71d8e11bcaff08cbf1
yugp0208 <yuu.fepnhdper2000@gmail.com>
2023-10-07 13:02:12 +0900

    feat smoothscrollの実装

--- src/js/main.js
+++ src/js/main.js
@@ -1,93 +1,22 @@
 //scss
 import '../scss/style.scss'
 //js
-import SplitType from 'split-type'
 import { gsap } from "gsap";
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
-gsap.registerPlugin(PixiPlugin, MotionPathPlugin);
+gsap.registerPlugin(ScrollTrigger);//scrollTriggerをプラグインとして登録
 
-const fvText = new SplitType(".c-fv__text");
-const headerText = new SplitType(".c-header__text");
-const logo = new SplitType(".c-header__logo");
+const container = document.querySelector('#scroll-container');//要素取得
+const height = container.clientHeight;//containerの表示域の高さを計算する
+document.body.style.height = `${height}px`;//bodyに表示域の高さを格納
 
-const headerAnimation = () => {
-  /* ヘッダーリンクのアニメーション */
-  gsap.to(".c-header__text.--home .char", {
-    y: 0,
-    stagger: 0.1,
-    duration: 0.5,
-    ease: Power2.easeInOut,
-  });
-  gsap.to(".c-header__text.--aboutus .char", {
-    y: 0,
-    stagger: 0.1,
-    duration: 0.5,
-    ease: Power2.easeInOut,
-  });
-  gsap.to(".c-header__text.--service .char", {
-    y: 0,
-    stagger: 0.1,
-    duration: 0.5,
-    ease: Power2.easeInOut,
-  });
-  gsap.to(".c-header__text.--company .char", {
-    y: 0,
-    stagger: 0.1,
-    duration: 0.5,
-    ease: Power2.easeInOut,
-  });
-  gsap.to(".c-header__text.--contact .char", {
-    y: 0,
-    stagger: 0.1,
-    duration: 0.5,
-    ease: Power2.easeInOut,
-  });
-};
-
-const tl = gsap.timeline();
-/*1.キャッチコピーのテキストアニメーション*/
-tl.to(fvText.chars, {
-  y: 0,
-  stagger: 0.05,
-  duration: 1,
-  ease: Power2.easeInOut,
-})
-  /*2.ロゴのテキストアニメーション*/
-  .fromTo(
-    logo.chars,
-    {
-      autoAlpha: 0,
-    },
-    {
-      autoAlpha: 1,
-      stagger: 0.05,
-      duration: 1,
-      ease: Power2.easeInOut,
-    }
-  )
-  /*3.ヘッダーのテキストリンクのテキストアニメーション*/
-  .call(headerAnimation)
-  /*4.テキストの表示*/
-  .to(".js-line-text", {
-    delay: 0.5,
-    autoAlpha: 1,
-    duration: 2,
-    ease: Power2.easeOut,
-  })
-  /*5.ラインアニメーションの表示*/
-  .to(".js-line", {
-    autoAlpha: 1,
-  })
-  /*6.背景画像の表示*/
-  .fromTo(
-    ".c-fv__bg",
-    {
-      autoAlpha: 1,
-    },
-    {
-      duration: 1,
-      autoAlpha: 0.2,
-      ease: Power2.easeOut,
-    },
-    ">-1" /*前のアニメーションが終わる1秒前にアニメーションの開始*/
-  );
+gsap.to(container, {
+  y: -(height - document.documentElement.clientHeight),
+  ease: 'none',
+  scrollTrigger: {
+    trigger: document.body,
+    start: 'top top',
+    end: 'bottom bottom',
+    scrub: 1,
+  },
+});
