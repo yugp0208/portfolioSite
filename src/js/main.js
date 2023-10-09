3a32b78cd420be604f2de0a4fe54137aef6ad457
yugp0208 <yuu.fepnhdper2000@gmail.com>
2023-10-09 14:52:43 +0900

    feat openingAnimationの実装

--- src/js/main.js
+++ src/js/main.js
@@ -3,20 +3,127 @@ import '../scss/style.scss'
 //js
 import { gsap } from "gsap";
 import ScrollTrigger from 'gsap/ScrollTrigger';
+/*register*/
+gsap.registerPlugin(ScrollTrigger);
 
-gsap.registerPlugin(ScrollTrigger);//scrollTriggerをプラグインとして登録
 
-const container = document.querySelector('#scroll-container');//要素取得
-const height = container.clientHeight;//containerの表示域の高さを計算する
-document.body.style.height = `${height}px`;//bodyに表示域の高さを格納
+//notScroll
+function disableScroll() {
+  document.body.classList.add('no-scroll');
+}
+function enableScroll() {
+  document.body.classList.remove('no-scroll');
+}
 
-gsap.to(container, {
-  y: -(height - document.documentElement.clientHeight),
-  ease: 'none',
-  scrollTrigger: {
-    trigger: document.body,
-    start: 'top top',
-    end: 'bottom bottom',
-    scrub: 1,
-  },
-});
+//loader
+const loaderAnimation = () => {
+  const loaderBg = document.querySelector('.js-loader-bg');
+  const loaderName = document.querySelector('.js-loader-name');
+  const header = document.querySelector(".header");
+  const aboutMainTxt = document.querySelector(".about__ttl");
+  const aboutSubTxt = document.querySelector(".about__feature");
+  const aboutImg = document.querySelector(".about__img");
+
+  disableScroll();
+
+  gsap.set(header,{
+    opacity:0,
+    y: -30,
+  })
+  gsap.set(aboutMainTxt,{
+    opacity:0,
+    y: 50,
+  })
+  gsap.set(aboutSubTxt,{
+    opacity:0,
+    y: 50,
+  })
+  gsap.set(aboutImg,{
+    opacity:0,
+    y: 50,
+  })
+  gsap.set(loaderName,{
+    opacity:0,
+    y:50,
+  })
+
+  //AnimationStart
+  const tl = gsap.timeline();
+  tl.to(
+    loaderName,
+    {
+      opacity: 1,
+      y: 50,
+      duration: 2,
+    }
+  )
+  .to(loaderName,{
+    opacity:0,
+    duration: 2,
+  })
+  .to(loaderBg,{
+    y:"-100%",
+    duration:1,
+    ease: "Power4.easeOut"
+  })
+  .to(header,{
+    opacity:1,
+    y:0,
+  })
+  .to(aboutMainTxt,{
+    opacity:1,
+    y:0,
+    onComplete:() => {
+      enableScroll();
+    }
+  })
+  .to(aboutSubTxt,{
+    opacity:1,
+    y:0,
+  })
+  .to(aboutImg,{
+    opacity:1,
+    y:0
+  })
+}
+
+//drawer
+const drawer = () => {
+  const drawerbtn = document.querySelector("#js-buttonHamburger");
+  const menu = document.querySelector(".menu");
+  const body = document.querySelector('body');
+  
+  gsap.set(menu,{
+    y:'-108%',
+    opacity:1,
+  })
+
+  const tl = gsap.timeline();
+
+  //drawer発火ボタンの属性切り替え
+  drawerbtn.addEventListener("click",function(){
+    body.classList.toggle('is-drawerActive');
+    this.classList.toggle('is-drawerActive');
+    tl.to(
+      menu,{
+      autoAlpha: 1,
+      duration: 1,
+      opacity:1,
+      y:"-5%",
+      ease: "Power4.easeOut"
+    })
+    if(this.getAttribute("aria-expanded") === "false"){
+      this.setAttribute("aria-expanded","true");
+      tl.play();
+      disableScroll();
+    }
+    else{
+      this.setAttribute("aria-expanded","false");
+      tl.reverse();
+      enableScroll();
+    }
+  })
+}
+
+drawer();
+loaderAnimation();
